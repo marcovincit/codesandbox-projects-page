bf867ac66864fda15358e0adc594553e06adce38
Marco Vincit <marcovincit@gmail.com>
2022-02-04 04:40:08 +0000

    Update Product.js

--- components/intro/Product.js
+++ components/intro/Product.js
@@ -1,7 +1,5 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { styled } from "styles/stitches.config";
-
-
 
 export const Canvas = styled("canvas", {
   maxWidth: "100vw",
@@ -14,59 +12,67 @@ export const Canvas = styled("canvas", {
   left: "50%",
 
   transform: "translate(-50%, -50%)",
-
-
 });
 
 export const Product = () => {
+  const canvasRef = useRef();
 
-  const canvas = useRef();
+  useEffect(() => {
+    // maxFrames
+    const maxFrames = 147;
 
-  useEffect(()=>{
-    const context = canvas.current.getContext("2d");
+    // canvas
+    const canvas = canvasRef.current;
 
-  const frameCount = 148;
-  const currentFrame = index => (
-    `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
+    // size
+    canvas.width = 1158;
+    canvas.height = 770;
+
+    // context
+    const context = canvas.getContext("2d");
+
+    // img
+    const img = new Image();
+
+    img.src =
+      "https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/0001.jpg";
+
+    img.onload = function () {
+      context.drawImage(img, 0, 0);
+    };
+
+    const updateImage = (index) => {
+      img.src = `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+        .toString()
+        .padStart(4, "0")}.jpg`;
+      context.drawImage(img, 0, 0);
+    };
+
+    window.addEventListener("scroll", () => {
+      const scrollY = window.scrollY;
+      const maxScrollTop = 1080 * 3;
+      const scrollFraction = scrollY / maxScrollTop;
+      const frameIndex = Math.min(
+        maxFrames - 1,
+        Math.ceil(scrollFraction * maxFrames)
+      );
+
+      requestAnimationFrame(() => updateImage(frameIndex + 1));
+    });
+
+    // const preloadImages = () => {
+    //   for (let i = 1; i < maxFrames; i++) {
+    //     const img = new Image();
+    //     img.src = currentImage(i);
+    //   }
+    // };
+
+    // preloadImages();
+  }, []);
+
+  return (
+    <>
+      <Canvas ref={canvasRef}></Canvas>
+    </>
   );
-  
-  const preloadImages = () => {
-    for (let i = 1; i < frameCount; i++) {
-      const img = new Image();
-      img.src = currentFrame(i);
-    }
-  };
-
-  const img = new Image()
-img.src = currentFrame(1);
-canvas.current.width=1158;
-canvas.current.height=770;
-img.onload=function(){
-  context.drawImage(img, 0, 0);
-}
-
-const updateImage = index => {
-  img.src = currentFrame(index);
-  context.drawImage(img, 0, 0);
-}
-
-
-window.addEventListener('scroll', () => {  
-  const scrollTop = window.scrollTop;
-  const maxScrollTop = 1080 * 13;
-  const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(
-    frameCount - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  
-  requestAnimationFrame(() => updateImage(frameIndex + 1))
-});
-
-preloadImages()
-  },[])
-
-  
-
-  return <Canvas ref={canvas}></Canvas>;
 };
